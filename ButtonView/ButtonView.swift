95d234bada68209d1ec97bf58d478b176a868169
osanai <osanai@CR-040.local>
2018-01-11 12:20:16 +0900

    first settings

---
+++ ButtonView/ButtonView.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonView.swift
+//  ButtonView
+//
+//  Created by osanai on 2018/01/11.
+//  Copyright © 2018年 jp.co.osanai.buttonview.demo. All rights reserved.
+//
+
+import UIKit
+
+class ButtonView: UIControl {
+
+}
