e7d1a06d0266c9a3d029e27ba13a35594fbdb306
osanai <osanai@cr-040.local>
2018-01-11 11:46:14 +0900

    Initial Commit

---
+++ ButtonView/ButtonView.h
@@ -0,0 +1,19 @@
+//
+//  ButtonView.h
+//  ButtonView
+//
+//  Created by osanai on 2018/01/11.
+//  Copyright © 2018年 jp.co.osanai.buttonview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ButtonView.
+FOUNDATION_EXPORT double ButtonViewVersionNumber;
+
+//! Project version string for ButtonView.
+FOUNDATION_EXPORT const unsigned char ButtonViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ButtonView/PublicHeader.h>
+
+
